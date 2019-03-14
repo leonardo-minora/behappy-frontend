53b37bac2fcd642aa99dc61afb4ea57a8ff96c0e
Leonardo MINORA <leonardo.minora@gmail.com>
2019-03-14 10:22:47 -0300

    Aula de estado/props do App React

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './index.css';
 export default function Header() {
   return (
     <div className="header pure-menu pure-menu-horizontal pure-menu-fixed">
-      <a href="/"><img className="logo" src={require("./img/logo.png")} /></a>
+      <a href="/"><img alt="Logo" className="logo" src={require("./img/logo.png")} /></a>
       <h4 className="label">Agenda de gentileza</h4>
     </div>
   )
---
+++ src/components/Input/Input.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Input(props) {
+  const style = {
+    borderColor: props.invalidValue ? '#d50000' : '#cccccc',
+    backgroundColor: props.invalidValue ? '#ffcdd2' : '#ffffff'
+  }
+  let properties = Object.assign({}, props);
+  delete properties.invalidValue;
+
+  return (
+    <input
+      type="text"
+      style={style}
+      {...properties}
+    />
+  );
+}
